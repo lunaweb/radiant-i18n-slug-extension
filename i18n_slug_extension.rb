18a82d1e00c3b85aeb26a0181b5dd5bfdd535989
Aimerick DESDOIT <aimerick@lunaweb.fr>
2010-10-27 13:46:26 +0200

    delete comments and add url

--- i18n_slug_extension.rb
+++ i18n_slug_extension.rb
@@ -1,24 +1,9 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application_controller'
-
 class I18nSlugExtension < Radiant::Extension
   version "1.0"
   description "i18n support for slugs"
-  url "http://yourwebsite.com/i18n_slug"
-  
-  # extension_config do |config|
-  #   config.gem 'some-awesome-gem
-  #   config.after_initialize do
-  #     run_something
-  #   end
-  # end
-
-  # See your config/routes.rb file in this extension to define custom routes
+  url "http://github.com/lunaweb/radiant-i18n-slug-extension"
   
   def activate
     Admin::PagesController.send :include, I18nSlugInterface
-    # tab 'Content' do
-    #   add_item "I18n Slug", "/admin/i18n_slug", :after => "Pages"
-    # end
   end
 end
