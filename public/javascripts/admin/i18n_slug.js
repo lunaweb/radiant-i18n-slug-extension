f49fe8dc875ec156931edd13f5433f7618fde8df
Stefan Husch <s.husch@qutic.com>
2010-11-10 15:21:02 +0100

    js refactor and add german umlauts

--- public/javascripts/admin/i18n_slug.js
+++ public/javascripts/admin/i18n_slug.js
@@ -1,26 +1,32 @@
-var slugify = function(str, replace){
-    var pattern = [
-        new RegExp('Ç', 'g'),
-        new RegExp('ç', 'g'),
-        new RegExp('[üûúù]', 'g'),
-        new RegExp('[éêëè]', 'g'),
-        new RegExp('[âäàåá]', 'g'),
-        new RegExp('[ïîìí]', 'g'),
-        new RegExp('[ÄÅ]', 'g'),
-        new RegExp('É', 'g'),
-        new RegExp('æ', 'g'),
-        new RegExp('Æ', 'g'),
-        new RegExp('[ôöòó]', 'g'),
-        new RegExp('Ü', 'g'),
-        new RegExp('ÿ', 'g'),
-        new RegExp('Ö', 'g'),
-        new RegExp('ñ', 'g'),
-        new RegExp('Ñ', 'g')
-    ];
-    var repl = ['C', 'c', 'u', 'e', 'a', 'i', 'A', 'E', 'ae', 'Ae', 'o', 'U', 'y', 'O', 'n', 'N'];
-
-    for (var i=0; i<pattern.length; i++) {
-        str = str.replace(pattern[i], repl[i]);
+var slugify = function(str){
+  
+    var items = {
+      'Ç'       : 'C',
+      'ç'       : 'c',
+      '[üûúù]'  : 'u',
+      '[éêëè]'  : 'e',
+      '[âàåá]'  : 'a',
+      '[ïîìí]'  : 'i',
+      '[Å]'     : 'A',
+      'É'       : 'E',
+      'æ'       : 'ae',
+      'Æ'       : 'Ae',
+      '[ôòó]'   : 'o',
+      'Ä'       : 'Ae',
+      'Ö'       : 'Oe',
+      'Ü'       : 'Ue',
+      'ä'       : 'ae',
+      'ö'       : 'oe',
+      'ü'       : 'ue',
+      'ß'       : 'ss',
+      'ÿ'       : 'y',
+      'Ö'       : 'O',
+      'ñ'       : 'n',
+      'Ñ'       : 'N'
+    };
+    
+    for(var index in items) {
+      str = str.replace(new RegExp(index, 'g'), items[index]);
     }
     
     return str;
@@ -28,8 +34,7 @@ var slugify = function(str, replace){
 
 Object.extend(String.prototype, {
   toSlug: function() {
-	var slug = slugify(this.strip().downcase());
-	slug = slug.replace(/[^-a-z0-9~_]/g, '-').replace(/-+/g, '-').replace(/^-/, '').replace(/-$/, '');
-    return slug;
+    var slug = slugify(this.strip().downcase());
+    return slug.replace(/[^-a-z0-9~_]/g, '-').replace(/-+/g, '-').replace(/^-/, '').replace(/-$/, '');
   }
 });
